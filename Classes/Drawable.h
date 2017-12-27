1e1fbddb2833b7d14cda134427de78a35710d8fc
Jean Tessier <jeant@clemson.edu>
2017-12-27 15:31:01 -0500

    code readability

--- Classes/Drawable.h
+++ Classes/Drawable.h
@@ -7,6 +7,7 @@
 class Drawable
 {
 	public:
+    // Constructors
     Drawable() : _sprite() { }
     Drawable(const std::string& path) :
       _sprite(cocos2d::Sprite::create(path))
@@ -14,26 +15,27 @@ class Drawable
     Drawable(const Drawable& draw) :
       _sprite(draw._sprite)
     { }
+    // -----
+
+    // GETTERS AND SETTERS
+    const cocos2d::Node* getNode() const;
+    cocos2d::Sprite* getSprite() const { return _sprite; }
+		cocos2d::Rect	getBoundingBox() const { return _sprite->getTextureRect(); }
+    cocos2d::Point getPosition() const { return _sprite->getPosition(); }
+		//cocos2d::Size	getContentSize();
+		//cocos2d::Point	getPosition();
+    //cocos2d::Vec2 getVelo() const { return _velocity; }
+		void setPosition(const cocos2d::Point& pt) { _sprite->setPosition(pt); }
+    // -----
+
+    void createSprite(const std::string& path) { _sprite = cocos2d::Sprite::create(path); }
 
     //virtual void update(float dt) { CCLOG("%s", "DRAW"); }
     virtual void update(float dt) = 0;
     virtual bool collidesWith(const Drawable* drawable) const;
 
-    const cocos2d::Node* getNode() const;
-    cocos2d::Sprite* getSprite() const { return _sprite; }
-    //cocos2d::Vec2 getVelo() const { return _velocity; }
-
-    void createSprite(const std::string& path) { _sprite = cocos2d::Sprite::create(path); }
-
     void retain();
     void release();
-
-		void setPosition(const cocos2d::Point& pt) { _sprite->setPosition(pt); }
-		//cocos2d::Point	getPosition();
-
-		cocos2d::Rect	getBoundingBox() const { return _sprite->getTextureRect(); }
-    cocos2d::Point getPosition() const { return _sprite->getPosition(); }
-		//cocos2d::Size	getContentSize();
 
 		//Now for the fun methods
 		//cocos2d::Point	moveFrom(cocos2d::Point);
@@ -42,7 +44,6 @@ class Drawable
 		//cocos2d::Point	moveRight();
 		//cocos2d::Point	moveUp();
 		//cocos2d::Point	moveDown();
-
 
 	private:
 		cocos2d::Sprite* _sprite;
